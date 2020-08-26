e8d16c724362951732fc36c8510350a4df4a551d
“jy8474np” <jy8474np@go.minnstate.edu>
2020-08-25 23:42:03 -0500

    camelCase with links and note

---
+++ camel_case.py
@@ -0,0 +1,12 @@
+
+# Resources
+# https://www.xspdf.com/resolution/50437614.html
+# https://www.includehelp.com/python/convert-a-string-to-camelcase.aspx
+# https://stackoverflow.com/questions/50437614/python-convert-a-string-from-any-format-into-camelcase
+# https://anothercodingblog.com/2020/03/04/daily-coding-challenge-convert-string-to-camel-case-written-in-javascript-and-python/
+
+# I found all of the above re: how to convert a string to camelCase, but
+# I honestly couldn't follow what was happening or why. Rather than copy
+# and paste without knowing why the code works, I figured I would provide 
+# the links and request that we review this conversion process in our next 
+# class. Thank you!
